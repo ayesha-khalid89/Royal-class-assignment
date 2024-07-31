69805a9c9bf4b16e2c9bb603ee482dac66e0f880
ayesha-khalid89 <159626121+ayesha-khalid89@users.noreply.github.com>
2024-07-31 15:03:24 +0400

    refactoring

--- assignment/app/page.js
+++ assignment/app/page.js
@@ -1,9 +1,9 @@
-import Image from "next/image";
-import styles from "./page.module.css";
 
+import Footer from "@components/Footer.jsx";
 export default function Home() {
   return (
     <>
+    <Footer />
     </>
   );
 }
---
+++ assignment/components/Footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./Footer.module.css";
+
+const Footer = () => {
+  return (
+    <div className='footer-main'>Footer</div>
+  )
+}
+
+export default Footer;
