77b0820eb1cf54a09f17ca5108c1d066338af8c2
ayesha-khalid89 <khalidayesha912@gmail.com>
2024-07-31 23:02:45 +0400

    style fix

--- assignment/components/imageDisplay/ImageDisplay.jsx
+++ assignment/components/imageDisplay/ImageDisplay.jsx
@@ -3,7 +3,7 @@ import styles from "./ImageDisplay.module.css";
 
 const ImageDisplay = () => {
   return (
-    <div className={styles.container}>
+    <div className={styles.image}>
         <img src="Frame-600.svg" alt="image"  className={styles.image}/>
     </div>
   )
