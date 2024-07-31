bcf0d3a76f197a5535757b0477a56ab32c47fd73
ayesha-khalid89 <khalidayesha912@gmail.com>
2024-07-31 16:48:36 +0400

    Category section

--- assignment/app/page.js
+++ assignment/app/page.js
@@ -2,11 +2,13 @@ import FeaturesSection from "@/components/FeaturesSection";
 import Footer from "../components/Footer";
 import Navbar from "@/components/Navbar";
 import Header from "@/components/Header";
+import CategorySection from "@/components/CategorySection";
 export default function Home() {
   return (
     <>
       <Header/>
       <Navbar />
+      <CategorySection />
       <FeaturesSection />
       <Footer />
     </>
---
+++ assignment/components/CategorySection.jsx
@@ -0,0 +1,45 @@
+"use client"
+import React, { useState, useEffect } from 'react';
+import styles from './Category.module.css';
+
+const CategorySection = () => {
+  const [categories, setCategories] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const categoriesPerPage = 6;
+
+  useEffect(() => {
+    fetch('https://api.escuelajs.co/api/v1/categories')
+      .then((response) => response.json())
+      .then((data) => setCategories(data));
+  }, []);
+
+  // Get current categories
+  const indexOfLastCategory = currentPage * categoriesPerPage;
+  const indexOfFirstCategory = indexOfLastCategory - categoriesPerPage;
+  const currentCategories = categories.slice(indexOfFirstCategory, indexOfLastCategory);
+
+  // Change page
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.header}>
+        <h2>Browse By Category</h2>
+        <div className={styles.pagination}>
+            <button onClick={() => paginate(currentPage - 1)} disabled={currentPage === 1}>❮</button>
+            <button onClick={() => paginate(currentPage + 1)} disabled={currentPage === Math.ceil(categories.length / categoriesPerPage)}>❯</button>
+        </div>
+      </div>
+      <div className={styles.categoryList}>
+        {currentCategories.map((category) => (
+          <div key={category.id} className={styles.categoryItem}>
+            <img src={category.image} alt={category.name} className={styles.categoryImage} />
+            <p>{category.name}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default CategorySection;
