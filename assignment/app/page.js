1f4042bd2a3348e295eca7f442125e8a4cbdd1d1
ayesha-khalid89 <khalidayesha912@gmail.com>
2024-07-31 16:18:31 +0400

    header

--- assignment/app/page.js
+++ assignment/app/page.js
@@ -1,9 +1,11 @@
 import FeaturesSection from "@/components/FeaturesSection";
 import Footer from "../components/Footer";
 import Navbar from "@/components/Navbar";
+import Header from "@/components/Header";
 export default function Home() {
   return (
     <>
+      <Header/>
       <Navbar />
       <FeaturesSection />
       <Footer />
