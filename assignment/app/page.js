b29ef8ad5e9ddee302f4495ff5d9abd932f7ed1f
ayesha-khalid89 <159626121+ayesha-khalid89@users.noreply.github.com>
2024-07-31 17:04:51 +0400

    new arrivals

--- assignment/app/page.js
+++ assignment/app/page.js
@@ -3,6 +3,8 @@ import Footer from "../components/Footer";
 import Navbar from "@/components/Navbar";
 import Header from "@/components/Header";
 import CategorySection from "@/components/CategorySection";
+=======
+import NewArrivalSection from "@/components/NewArrivals";
 export default function Home() {
   return (
     <>
---
+++ assignment/components/NewArrivals.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import styles from "./NewArrivals.module.css";
+
+const NewArrivalSection = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.header}>
+        <div>
+          <button className={styles.button}></button>
+          <span className={styles.featured}>Featured</span>
+        </div>
+
+        <h2 className={styles.title}>New Arrival</h2>
+      </div>
+      <div className={styles.grid}>
+        <div className={styles.largeItem}>
+          <img
+            src="Frame-739.svg"
+            alt="PlayStation 5"
+            className={styles.image}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NewArrivalSection;
